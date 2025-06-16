abc022038b46bb1acf968245576d297ff1f5454e
teecoleonard <leonardo4q@gmail.com>
2025-06-16 15:41:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.pokeapp',
+  appName: 'pokeapp',
+  webDir: 'www'
+};
+
+export default config;
